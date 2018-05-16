9394b421c45d2bc398f32fa3e667bb149d964a1b
summer2023 <happyxia1023@163.com>
2018-05-16 19:15:31 +0800

    write Inventory.entity

---
+++ src/main/java/com/example/jinxiMall/entity/Inventory.java
@@ -0,0 +1,46 @@
+package com.example.jinxiMall.entity;
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+public class Inventory {
+    @Id
+    private Long id;
+    private Integer count;
+    private Integer lockedCount;
+    private Product product;
+
+    public Inventory() {
+    }
+
+    public Inventory(Long id, Integer count, Integer lockedCount) {
+        this.id = id;
+        this.count = count;
+        this.lockedCount = lockedCount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Integer getLockedCount() {
+        return lockedCount;
+    }
+
+    public void setLockedCount(Integer lockedCount) {
+        this.lockedCount = lockedCount;
+    }
+}
