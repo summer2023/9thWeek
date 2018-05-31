5bd48415d62a8ccaf8eedb52c4ad269d94d3ebb0
summer2023 <happyxia1023@163.com>
2018-05-31 17:35:49 +0800

    write productsnap

---
+++ src/main/java/com/example/jinxiMall/Repository/ProductSnapRepository.java
@@ -0,0 +1,18 @@
+package com.example.jinxiMall.Repository;
+
+import com.example.jinxiMall.entity.ProductSnap;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public interface ProductSnapRepository extends JpaRepository<ProductSnap, Long> {
+    //创建新商品
+    @Transactional
+    ProductSnap save(ProductSnap productSnap);
+
+    //根据订单id查找库存
+    List<ProductSnap> findProductSnapByOrderId(Long orderId);
+}
