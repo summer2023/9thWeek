28d9bae794e14124de3cad717122a32531745196
summer2023 <happyxia1023@163.com>
2018-06-01 11:26:32 +0800

    correct deliveryFormController

--- src/main/java/com/example/jinxiMall/controller/DeliveryFormController.java
+++ src/main/java/com/example/jinxiMall/controller/DeliveryFormController.java
@@ -3,7 +3,10 @@ package com.example.jinxiMall.controller;
 import com.example.jinxiMall.Repository.DeliveryFormRepository;
 import com.example.jinxiMall.Repository.InventoryRepository;
 import com.example.jinxiMall.Repository.OrderRepository;
+import com.example.jinxiMall.Repository.ProductSnapRepository;
 import com.example.jinxiMall.entity.DeliveryForm;
+import com.example.jinxiMall.entity.ProductSnap;
+import com.example.jinxiMall.exceptions.ItemNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
@@ -21,13 +24,15 @@ public class DeliveryFormController {
     @Autowired
     private OrderRepository orderRepository;
     @Autowired
+    private ProductSnapRepository productSnapRepository;
+    @Autowired
     private InventoryRepository inventoryRepository;
 
     //根据订单id查找物流
     @GetMapping("{id}")
     @ResponseStatus(HttpStatus.OK)
-    public DeliveryForm getLogisticsRecord(@PathVariable Long id){
-        DeliveryForm deliveryForm = deliveryFormRepository.findDeliveryFormById(id);
+    public DeliveryForm getLogisticsRecord(@PathVariable Long id) throws Exception {
+        DeliveryForm deliveryForm = deliveryFormRepository.findById(id).orElseThrow(() -> new ItemNotFoundException("logisticsRecord", id));
         return deliveryForm;
     }
 
@@ -35,16 +40,21 @@ public class DeliveryFormController {
     @PutMapping("{id}/orders/{orderId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void updateOrderStatus(@PathVariable Long id, @PathVariable Long orderId, @RequestParam String logisticsStatus) throws Exception {
-        DeliveryForm deliveryForm = deliveryFormRepository.findByIdAndOrderId(id, orderId);
+       DeliveryForm deliveryForm = deliveryFormRepository.findByIdAndOrderId(id, orderId);
         String nowDate = String.valueOf(new Date(System.currentTimeMillis()));
-
-        final boolean isDeliveryInfoAlreadyShippedOrSigned = deliveryForm.getLogisticsStatus().equals("shipping") || deliveryForm.getLogisticsStatus().equals("signed");
-        updateOrderStatusAndInventories(orderId, nowDate);
+        if (deliveryForm == null) {
+            throw new ItemNotFoundException("logisticsRecord", "logisticsId", id, "orderId", orderId);
+        }
+        final boolean isLogisticsAlreadyShippedOrSigned = deliveryForm.getLogisticsStatus().equals("shipping") || deliveryForm.getLogisticsStatus().equals("signed");
+        if (logisticsStatus.equals("signed")) {
+            updateOrderStatusAndInventories(orderId, nowDate);
+        }
         updateLogisticsStatus(logisticsStatus, id, orderId, nowDate);
     }
 
     private void updateOrderStatusAndInventories(Long orderId, String nowDate) {
         orderRepository.updateOrderStatusToFinished(orderId, "finished", nowDate);
+        updateInventoriesAfterSignedOff(orderId);
     }
 
     private void updateLogisticsStatus(String logisticsStatus, Long id, Long orderId, String nowDate) {
@@ -56,5 +66,22 @@ public class DeliveryFormController {
 
     }
 
+    private String checkWhetherCanSignLogistics(DeliveryForm deliveryForm) {
+        if (deliveryForm.getLogisticsStatus().equals("readyToShip")) {
+            return "The logisticsRecord hasn't been shipped yet.";
+        }
+        if (deliveryForm.getLogisticsStatus().equals("signed")) {
+            return "The logisticsRecord has already been signed off.";
+        }
+        return "success";
+    }
+
+    private void updateInventoriesAfterSignedOff(Long orderId) {
+        List<ProductSnap> products = productSnapRepository.findProductSnapByOrderId(orderId);
+        for (ProductSnap product : products) {
+            inventoryRepository.updateCountById(product.getId(), -product.getPurchaseCount());
+            inventoryRepository.updateLockedCount(product.getId(), -product.getPurchaseCount());
+        }
+    }
 
 }
